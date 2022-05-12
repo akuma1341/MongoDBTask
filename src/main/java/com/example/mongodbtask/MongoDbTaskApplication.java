a1def5410f2afd81f3efb263e736ca871ff94c43
Raman Rashchupkin <rrashchupkin@piesoft.us>
2022-05-12 13:55:07 +0300

    Initial commit

---
+++ src/main/java/com/example/mongodbtask/MongoDbTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodbtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mongodbtask/MongoDbTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodbtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
